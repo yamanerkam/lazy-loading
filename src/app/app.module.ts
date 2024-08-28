e533f0fda5d5adac9b499f551914873842dec624
yamanerkam <erkamyaman35@gmail.com>
2024-08-28 14:27:04 +0300

    routing-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListProductComponent } from './components/products/list-product/list-pr
 import { AddCustomerComponent } from './components/customer/add-customer/add-customer.component';
 import { ListCustomerComponent } from './components/customer/list-customer/list-customer.component';
 import { DetailCustomerComponent } from './components/customer/detail-customer/detail-customer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home/home.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { DetailCustomerComponent } from './components/customer/detail-customer/d
     ListProductComponent,
     AddCustomerComponent,
     ListCustomerComponent,
-    DetailCustomerComponent
+    DetailCustomerComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
