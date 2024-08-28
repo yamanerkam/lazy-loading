5e0837807b91d05c2dd272d1da2b25adeba440e2
yamanerkam <erkamyaman35@gmail.com>
2024-08-28 14:04:19 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './components/products/add-product/add-product.component';
+import { DetailProductComponent } from './components/products/detail-product/detail-product.component';
+import { ListProductComponent } from './components/products/list-product/list-product.component';
+import { AddCustomerComponent } from './components/customer/add-customer/add-customer.component';
+import { ListCustomerComponent } from './components/customer/list-customer/list-customer.component';
+import { DetailCustomerComponent } from './components/customer/detail-customer/detail-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    DetailProductComponent,
+    ListProductComponent,
+    AddCustomerComponent,
+    ListCustomerComponent,
+    DetailCustomerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/customer/detail-customer/detail-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-customer',
+  templateUrl: './detail-customer.component.html',
+  styleUrls: ['./detail-customer.component.css']
+})
+export class DetailCustomerComponent {
+
+}
